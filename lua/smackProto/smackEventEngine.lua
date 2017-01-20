111479dfa09d50a3383cb1c837ad204701843a0a
jcrawford <jcrawford@idirectgov.com>
2017-01-20 16:25:12 -0500

    Put prototype stuff in folder, clean up

---
+++ lua/smackProto/smackEventEngine.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/env lua
+
+-- Simulated Event Engine
+-- Drive a little steam engine that pops out events
+
+-- Events to test smack talk should be something like
+-- SomethingAwesome
+-- SomethingCrappy
+-- SomethingCriticallyAwesome
+-- SomethingCriticallyCrappy
+
+-- Yeah that will be a fine start
+
